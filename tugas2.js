d8cc38658bc5c563a1e8594496b7953ebd82efda
oniflestari <onifahmilestari@gmail.com>
2022-07-19 12:32:47 +0700

    Tugas 2 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+// tipe data number
+var varnumber = 18;
+
+// tipe data string
+var varstring = 'programmer';
+
+// tipe data boolean
+var varboolean = true;
+
+// tipe data null/undefined
+var varundefined;
+
+// tipe data array
+var vararray = ['oni', 23, 'the best programmer']
+
+console.log(varnumber, varstring, varboolean, varundefined, vararray)
